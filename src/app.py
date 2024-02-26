d0e5624eddae0e86fe46cc0cd184769035cd5fee
Chayathon.R <chayathon14639@gmail.com>
2024-02-26 19:46:12 +0700

    can_getall

--- src/app.py
+++ src/app.py
@@ -1,22 +1,37 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 from flask_cors import CORS
+from pymongo.mongo_client import MongoClient
 app = Flask(__name__)
+uri = "mongodb+srv://chayathon14639:GVlE571RSj3LCD1p@cluster0.76js1dj.mongodb.net/"
+client = MongoClient(uri)
+
+db = client["Week+10Db"]
+collection = db["product"]
 CORS(app)
-products=[
-{"id":0,"name":"Notebook Acer Swift","price":45900,"img":"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
-{"id":1,"name":"Notebook Asus Vivo","price":19900,"img":"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
-{"id":2,"name":"Notebook Lenovo Ideapad","price":32900,"img":"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
-{"id":3,"name":"Notebook MSI Prestige","price":54900,"img":"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
-{"id":4,"name":"Notebook DELL XPS","price":99900,"img":"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
-{"id":5,"name":"Notebook HP Envy","price":46900,"img":"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"}];
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+
 @app.route("/products",methods=["GET"])
 def get_all_products():
-    return jsonify(products),200
+    products = list(collection.find({}))
+    product_list = []
+
+    for product in products:
+        product_data = {
+            "id": str(product["id"]),
+            "name": product.get("name", ""),
+            "price": product.get("price", ""),
+            "img": product.get("img", "")
+        }
+        product_list.append(product_data)
+    
+    return product_list
+
+
 
 
 if __name__ == "__main__":
