d43a09bde5abba5de38a4afe04beb8c8aa4a61b1
Chayathon.R <chayathon14639@gmail.com>
2024-02-26 21:16:21 +0700

    Front_GetAllProduct MGDB

--- src/Product.js
+++ src/Product.js
@@ -1,30 +1,27 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// const Item = props =>
-// { 
-//     return (<div onClic k={e => props.callback(props.id)}>
-//         <img src={props.img} width={200} height={200} /><br/>
-//         id : {props.id}<br/>
-//         Name : {props.name}<br />
-//         Price : {props.price}<br/>    
-//     </div>);
-// }
-export default function Product()
-{
-    const [product,setProduct]=useState([])
-    useEffect(()=>{
-        console.log("request to api")
-        axios.get("http://127.0.0.1:5000/products")
-        .then(response=>setProduct(response.data))
-        .catch(error => {
-            console.error('Error fetching datas:', error);
-          })
-    },[])
-    const productList=product.map(p=><li key={p.id}>{p.id} 
-    {p.name} <img src={p.img}/> {p.price}</li>)
-    return (<>
-        <ul>
-            {productList}
-        </ul>
-    </>)
+
+export default function Product() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/products')
+            .then(response => setProducts(response.data.products))
+            .catch(error => console.error('Error fetching products:', error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Products </h1>
+            <ul>
+                {products.map(product => (
+                    <li key={product._id}>
+                        <img src={product.img} alt={product.name} />
+                        <div>{product.name}</div>
+                        <div>Price: {product.price}</div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
